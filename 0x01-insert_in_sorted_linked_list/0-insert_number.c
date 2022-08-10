be5f4153aa9fcd060c5982c5d8c7087cb34a663c
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-10 22:20:13 +0000

    given a sorted linked list, insert node. easy stuff

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,41 +1,48 @@
 #include "lists.h"
 
+/**
+ * insert_node - given sorted linked list, insert node in ascending order
+ * @head: pointer pointing to the pointer node
+ * @number: number to insert in new node
+ * Return: new node
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new = NULL, *aux = *head, *prev = NULL;
+	listint_t *new = NULL, *aux = *head, *prev = NULL;
 
-    new->n = number, new->next = NULL;
-    
-    if (!(*head)) 
-    {
-        *head = new;
-        return new;
-    }
-    if (!aux->next)
-    {
-        if (aux->n < number)
-        {
-            aux->next = new;
-            return new;
-        }
-        else
-        {
-            new->next = aux;
-            return new;
-        }
-    }
-    while (aux->next)
-    {
-        if (aux->next->n > number)
-            break;
-        prev = aux, aux = aux->next;
-    }
-    if (aux->n == number)
-    {
-        aux = new;
-        return new;
-    }
-    prex->next = new;
-    new->next = aux;
-    return new;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number, new->next = NULL;
+
+	if (!(*head))
+	{
+		*head = new;
+		return (new);
+	}
+	if (!aux->next)
+	{
+		if (aux->n < number)
+			aux->next = new;
+		else
+		{
+			new->next = aux;
+			*head = new;
+		}
+		return (new);
+	}
+	while (aux->next)
+	{
+		if (aux->n > number)
+			break;
+		prev = aux, aux = aux->next;
+	}
+	if (aux->n == number)
+	{
+		aux = new;
+		return (new);
+	}
+	prev->next = new;
+	new->next = aux;
+	return (new);
 }
