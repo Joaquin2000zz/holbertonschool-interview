aaa46b8b23a6bb2b5c87ab8bd616700637a1fd98
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-08-15 14:31:50 +0000

    function which inserts leafs. if the tree dosen't exist, create a root

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,18 +1,16 @@
 #include "binary_trees.h"
+#include <stdio.h>
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new = NULL;
 
-    if (!parent)
-        return (NULL);
-    new = malloc(sizeof(binary_tree_t *));
+    new = malloc(sizeof(binary_tree_t));
     if (!new)
         return (NULL);
     new->parent = parent;
     new->left = NULL;
     new->right = NULL;
     new->n = value;
-
     return (new);
 }
