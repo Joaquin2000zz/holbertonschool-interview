2d0ce58894b3b83e9732ba5fd72fdecbcb81a793
Joaquin Victoria <3942@holbertonstudents.com>
2022-08-26 10:52:24 -0300

    first successfully attempt

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+0-minoperations module which contains minOperations(n)
+"""
+
+
+def minOperations(n):
+    """
+    Given a number n, write a method that calculates the fewest number
+    of operations needed to result in exactly n H characters in the file.
+    
+    Returns an integer
+    If n is impossible to achieve, return 0
+    """
+
+    File = "H"
+    copy = None
+    On = 0
+    
+    i = 0
+    
+    flag = 1
+
+    while (1):
+        i += 2 if flag else 1
+
+        if flag:
+            copy = File
+            File += copy
+            flag = 0
+            On += 2
+        else:
+            File += copy
+            flag = 0 if len((File + 'H') * 2) > n else 1
+            On += 1
+        l = len(File)
+        if l > n:
+            print(File)
+            return 0
+        if l == n:
+            print(File)
+            return On
