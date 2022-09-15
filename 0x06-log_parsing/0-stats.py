51c57fa59398cb929ec259a4c29dd2f761fcf8ba
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-15 15:42:11 -0300

    first attempt

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,10 +11,32 @@ module which contains a script that reads stdin line by line and computes metric
     - where <total size> is the sum of all previous <file size>
 * Number of lines by status code:
     - possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-    - if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+    - if a status code doesn’t appear or is not an integer,
+      don’t print anything for this status code
     - format: <status code>: <number>
     - status codes should be printed in ascending order
 """
-from sys import argv
+import sys
 
-print(argv)
+
+try:
+    buffer = {}
+    size = 0
+    for i, line in enumerate(sys.stdin):
+        code, sizeS = line.split('"')[2][1:].split(' ')
+        size += int(sizeS)
+        if not buffer.get(code):
+            buffer[code] = 1
+        else:
+            buffer[code] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(size))
+            for k in sorted(buffer.keys()):
+                to_print = k + " " + str(buffer[k])
+                print(to_print)
+            buffer = {}
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+    for k in sorted(buffer.keys()):
+        to_print = k + " " + str(buffer[k])
+        print(to_print)
