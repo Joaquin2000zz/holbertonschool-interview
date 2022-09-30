ceb91a0f0a919f86fc16ab93ecb50a0221c49df9
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-09-29 23:00:34 -0300

    added header file

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,8 @@
+#ifndef _IS_PALINDROME_
+#define _IS_PALINDROME_
+
+unsigned long ndigits(unsigned long n);
+unsigned long _pow(unsigned long n, unsigned long k);
+int is_palindrome(unsigned long n);
+
+#endif /*_IS_PALINDROME_*/
