d0333290bb91e9e9c1835786f9a43330133609aa
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-11-11 02:18:54 +0000

    fixed betty codestyle

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -7,25 +7,26 @@
  * @array: pointer to the first element of the array to be converted
  * @size: number of element in the array
  * @parent: parent node or null in case of root node
+ * Return: root node
  */
 avl_t *rec(int *array, size_t size, avl_t *parent)
 {
-    binary_tree_t *node = NULL;
-    avl_t *root = NULL;
+	binary_tree_t *node = NULL;
+	avl_t *root = NULL;
 
-    node = malloc(sizeof(avl_t));
-    if (!node)
-        return (NULL);
-    root = node;
+	node = malloc(sizeof(avl_t));
+	if (!node)
+		return (NULL);
+	root = node;
 
-    if (!array || size == 0)
-        return (NULL);
-    root->parent = parent, root->left = NULL, root->right = NULL;
-    root->n = array[(size - 1) / 2];
-    root->left = rec(array, (size - 1) / 2, root);
-    root->right = rec(array + (size + 1) / 2, (size - 1) / 2, root);
+	if (!array || size == 0)
+		return (NULL);
+	root->parent = parent, root->left = NULL, root->right = NULL;
+	root->n = array[(size - 1) / 2];
+	root->left = rec(array, (size - 1) / 2, root);
+	root->right = rec(array + (size + 1) / 2, (size - 1) / 2, root);
 
-    return (root);    
+	return (root);
 }
 
 /**
@@ -33,11 +34,12 @@ avl_t *rec(int *array, size_t size, avl_t *parent)
  *
  * @array: pointer to the first element of the array to be converted
  * @size: number of element in the array
+ * Return: root node
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return (NULL);
+	if (!array || size == 0)
+		return (NULL);
 
-    return (rec(array, size, NULL));
+	return (rec(array, size, NULL));
 }
