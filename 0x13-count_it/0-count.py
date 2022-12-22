7e8d18d14d9529ca27ac0c9154df0a834c588076
Joaquin Victoria <3942@holbertonstudents.com>
2022-12-22 20:35:43 -0300

    using the same function to the recursion

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,7 +5,7 @@ module which contains count_words function
 import requests
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, i=0, n=None, data=None, result=None):
     """
     recursive function that queries the Reddit API, parses the title of all
     hot articles, and prints a sorted count of given keywords
@@ -14,46 +14,41 @@ def count_words(subreddit, word_list):
 
     """
 
-    r = requests.get('https://www.reddit.com/r/' + subreddit + '/hot.json',
-                     headers={'User-agent': 'your bot 0.1'})
-    data = r.json().get('data').get('children')
-    n = len(data)
-    result = {}
+    if not data:
+        r = requests.get('https://www.reddit.com/r/' + subreddit + '/hot.json',
+                         headers={'User-agent': 'your bot 0.1'})
+        data = r.json().get('data').get('children')
+        n = len(data)
+        result = {}
 
-    def req(data, i, n, result):
-        """
-        recursion needed
-        """
-        if i < n:
-            for word in word_list:
-                word = word.lower()
-                if word in data[i].get('data').get('title').lower():
-                    if not result.get(word):
-                        result[word] = 1
-                    else:
-                        result[word] += 1
+    if i < n:
+        for word in word_list:
+            word = word.lower()
+            if word in data[i].get('data').get('title').lower():
+                if not result.get(word):
+                    result[word] = 1
                 else:
-                    if not result.get(word):
-                        result[word] = 0
+                    result[word] += 1
+            else:
+                if not result.get(word):
+                    result[word] = 0
+        result = count_words(subreddit, word_list, i + 1, n, data, result)
 
-            return req(data, i + 1, n, result)
-        return result
-    result = req(data, 0, n, result)
+    if i == 0:
+        keys = list(result.keys())
+        keys.sort()
+        new_res = {}
+        for key in keys:
+            num = result.get(key)
+            if not new_res.get(num):
+                new_res[num] = [key]
+            else:
+                new_res[num].append(key)
 
-    keys = list(result.keys())
-    keys.sort()
-    new_res = {}
-    for key in keys:
-        num = result.get(key)
-        if not new_res.get(num):
-            new_res[num] = [key]
-        else:
-            new_res[num].append(key)
-
-    result = {}
-    for key in new_res:
-        for name in new_res.get(key):
-            print(name + ' :', key)
-            result[name] = key
+        result = {}
+        for key in new_res:
+            for name in new_res.get(key):
+                print(name + ' :', key)
+                result[name] = key
 
     return result
