f16230195373edee415bd706be5c9b286284a6ed
Joaquin Victoria <3942@holbertonstudents.com>
2022-12-22 21:33:27 -0300

    handled bordercase and fixed print message

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -14,7 +14,7 @@ def count_words(subreddit, word_list, next=None, result=None):
     """
 
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
-    if not next:
+    if not next and not result:
         r = requests.get(url,
                          headers={'User-agent': 'your bot 0.1'},
                          allow_redirects=False)
@@ -32,7 +32,7 @@ def count_words(subreddit, word_list, next=None, result=None):
         title_words = data.get('data').get('title').lower().split()
         for title in title_words:
             for word in word_list:
-                if title == word:
+                if title == word.lower():
                     result[title] += 1
 
     if next:
@@ -51,7 +51,7 @@ def count_words(subreddit, word_list, next=None, result=None):
         result = {}
         for key in new_res:
             for name in new_res.get(key):
-                print(name + ' :', key)
+                print(name + ':', key)
                 result[name] = key
 
     return result
